ca86c3e04dda551d290b73e9a35c0e21242a1c86
me_from_Office <55085135+navbhurli@users.noreply.github.com>
2020-05-13 17:20:49 +0530

    update

--- BlackAndVeatchScript/updatedScript.ps1
+++ BlackAndVeatchScript/updatedScript.ps1
@@ -44,38 +44,47 @@ $serverList = Import-Csv -Path "$PSScriptRoot\Serverlist.csv"
 
 foreach($remoteserver in $serverList)
 {
-    $rmsrvName = $remoteserver.Name
-    Input-Log "Scanning $rmsrvName"
+    #$rmsrvName = $remoteserver.Name
+    Input-Log "Scanning '$($remoteserver.Name)'"
     #remote session to the server/machine
-    $remoteSession = New-PSSession -Credential $credential -ComputerName $remoteServer.Name
-    foreach($user in $userList)
+    try 
     {
-        Input-Log "Checking if '$($user.lastname), $($user.firstname)' exists and removing it"
-        # fetching UserSamAccountName from provided user's first and last name from AD
-        $userSam = (Get-ADUser -Filter "name -like '$($user.Lastname)*, $($user.firstname)*'").samaccountname
-        # Remoting to that server and executing the script (further important actions)
-        Invoke-Command -Session $remoteSession -ScriptBlock {
-            
-            # fecthing all the users from local administrators group
-            $localAdminGroup = Get-LocalGroupMember -Group "Administrators"
-            # using the variable from outside the scriptblock
-            $userSamName = $Using:userSam
+        $remoteSession = New-PSSession -Credential $credential -ComputerName $remoteServer.Name
+        foreach($user in $userList)
+        {
+            Input-Log "Checking if '$($user.lastname), $($user.firstname)' exists and removing it"
+            # fetching UserSamAccountName from provided user's first and last name from AD
+            $userSam = (Get-ADUser -Filter "name -like '$($user.Lastname)*, $($user.firstname)*'").samaccountname
+            # Remoting to that server and executing the script (further important actions)
+            Invoke-Command -Session $remoteSession -ScriptBlock {
+                
+                # fecthing all the users from local administrators group
+                $localAdminGroup = Get-LocalGroupMember -Group "Administrators"
+                # using the variable from outside the scriptblock
+                $userSamName = $Using:userSam
 
-            # iterating through all the members and comparing the samaccountname
-            foreach($localAdminUser in $localAdminGroup.Name)
-            {
-                # if any user exusts then go ahead with removing it from the local admin group
-                if(${localAdminUser} -match $userSamName)
+                # iterating through all the members and comparing the samaccountname
+                foreach($localAdminUser in $localAdminGroup.Name)
                 {
-                    #Perform the required function i.e., Remove-LocalGroupMember
-                    Get-LocalGroupMember -Group "Administrators" -Member $localAdminUser
+                    # if any user exusts then go ahead with removing it from the local admin group
+                    if(${localAdminUser} -match $userSamName)
+                    {
+                        #Perform the required function i.e., Remove-LocalGroupMember
+                        Get-LocalGroupMember -Group "Administrators" -Member $localAdminUser
+                    }
+
                 }
 
-            }
 
-
-        }    
-    }    
+            }   
+        }
+    }
+    catch 
+    {
+        Input-Log "The server may not be available this moment and hence, it couldn't remove users from it"
+        
+    }
+        
+    # removing the PSSession
+    Remove-PSSession $remoteSession
 }
-# removing the PSSession
-Remove-PSSession $remoteSession
